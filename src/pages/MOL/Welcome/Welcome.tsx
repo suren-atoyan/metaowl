02a5677a4e381188c361278af46e1a8f92bf1309
suren-atoyan <contact@surenatoyan.com>
2022-07-23 15:44:33 +0400

    make trio size responsive

--- src/pages/MOL/Welcome/Welcome.tsx
+++ src/pages/MOL/Welcome/Welcome.tsx
@@ -1,9 +1,23 @@
+import { useMediaQuery } from 'react-responsive';
+
 import Box from '@mui/material/Box';
 
 import title from '@/assets/images/title.svg';
 import trio from '@/assets/images/trio.svg';
 
 function Welcome() {
+  const isBigScreen = useMediaQuery({
+    query: '(min-width: 1700px)',
+  });
+  const isMidScreen = useMediaQuery({
+    query: '(min-width: 820px)',
+  });
+  const isMobile = useMediaQuery({
+    query: '(max-width: 820px)',
+  });
+
+  const trioWidth = isBigScreen ? '30%' : isMidScreen ? '40%' : isMobile ? '70%' : '30%';
+
   return (
     <Box
       sx={{
@@ -19,7 +33,7 @@ function Welcome() {
       <img src={title} style={{ width: '84%', height: 'fit-content' }} />
       <img
         src={trio}
-        style={{ position: 'absolute', bottom: 0, right: 0, width: '30%', marginRight: '5%' }}
+        style={{ position: 'absolute', bottom: 0, right: 0, width: trioWidth, marginRight: '5%' }}
       />
     </Box>
   );
